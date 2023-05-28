4f03222ded7e045e53812cffe3d54a5ec114c163
a.krainova <a.krainova@rt-solar.ru>
2023-05-28 22:08:24 +0300

    Update plain.py

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -1,30 +1,28 @@
-def make_plain(diff, path=''):
-    def parse_value(value):
-        if isinstance(value, dict):
-            return "[complex value]"
+def _parse_value(value: str | dict) -> str:
+    if isinstance(value, dict):
+        return "[complex value]"
+    else:
+        if type(value) is bool:
+            return f"{value}".lower()
+        elif value is None:
+            return "null"
         else:
-            if type(value) is bool:
-                return f"{value}".lower()
-            elif value is None:
-                return "null"
-            else:
-                return f"'{value}'"
+            return f"'{value}'"
 
+
+def make_plain(diff, path=''):
     def make_str_for_unchanged():
         return ""
 
     def make_str_for_added(key, value):
-        if isinstance(value, dict):
-            return f"""\nProperty '{key}' was added with value: {parse_value(value)}"""
-        else:
-            return f"""\nProperty '{key}' was added with value: {parse_value(value)}"""
+        return f"\nProperty '{key}' was added with value: {_parse_value(value)}"
 
     def make_str_for_removed(key):
         return f"\nProperty '{key}' was removed"
 
     def make_str_for_updated(key, old_value, value):
         result = f"\nProperty '{key}' was updated. "
-        result += f"From {parse_value(old_value)} to {parse_value(value)}"
+        result += f"From {_parse_value(old_value)} to {_parse_value(value)}"
         return result
 
     result = ''
