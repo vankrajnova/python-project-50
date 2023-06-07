da5be4a1bcea2e18d3a1efc65c04f6d7d232cb52
a.krainova <a.krainova@rt-solar.ru>
2023-06-07 15:58:54 +0300

    update test scope

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -13,17 +13,11 @@ def _get_file_path_by(file_name: str) -> str:
     return f'{FIXTURES_DIR}{file_name}'
 
 
-JSON1 = _get_file_path_by('file1.json')
-JSON2 = _get_file_path_by('file2.json')
-YAML1 = _get_file_path_by('file1.yml')
-YAML2 = _get_file_path_by('file2.yml')
 NESTED_JSON1 = _get_file_path_by('nested_file1.json')
 NESTED_JSON2 = _get_file_path_by('nested_file2.json')
 NESTED_YAML1 = _get_file_path_by('nested_file1.yml')
 NESTED_YAML2 = _get_file_path_by('nested_file2.yml')
-FLAT_STYLISH_OUTPUT = _get_expected_output_by('flat_output.txt')
 NESTED_STYLISH_OUTPUT = _get_expected_output_by('nested_output.txt')
-FLAT_JSON_OUTPUT = _get_expected_output_by('flat_json_output.json')
 NESTED_JSON_OUTPUT = _get_expected_output_by('nested_json_output.json')
 PLAIN_OUTPUT = _get_expected_output_by('plain_output.txt')
 
@@ -31,25 +25,19 @@ PLAIN_OUTPUT = _get_expected_output_by('plain_output.txt')
 @pytest.mark.parametrize(
     'filepath1, filepath2, output, formatter',
     [
-        (JSON1, JSON2, FLAT_STYLISH_OUTPUT, 'stylish'),
-        (YAML1, YAML2, FLAT_STYLISH_OUTPUT, 'stylish'),
         (NESTED_JSON1, NESTED_JSON2, NESTED_STYLISH_OUTPUT, 'stylish'),
         (NESTED_YAML1, NESTED_YAML2, NESTED_STYLISH_OUTPUT, 'stylish'),
         (NESTED_JSON1, NESTED_JSON2, PLAIN_OUTPUT, 'plain'),
         (NESTED_YAML1, NESTED_YAML2, PLAIN_OUTPUT, 'plain'),
-        (JSON1, JSON2, FLAT_JSON_OUTPUT, 'json'),
         (NESTED_JSON1, NESTED_JSON2, NESTED_JSON_OUTPUT, 'json'),
     ],
     ids=[
-        'data=two flat json, format=stylish',
-        'data=two flat yml, format=stylish',
         'data=two nested json, format=stylish',
         'data=two nested yml, format=stylish',
 
         'data=two nested json, format=plain',
         'data=two nested yml, format=plain',
 
-        'data=two flat json, format=json',
         'data=two nested json, format=json'
     ],
 )
