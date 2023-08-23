93761c5d7badd214e36647a3eb7816984fdc41e6
a.krainova <a.krainova@rt-solar.ru>
2023-08-23 22:49:11 +0300

    fix stylish

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -1,50 +1,60 @@
-import json
+INDENT = '    '
+ADD = '  + '
+REMOVE = '  - '
+OPEN_BRACE = '{\n'
+CLOSE_BRACE = '}\n'
 
 
-def _convert_string(string: str) -> str:
-    data = {
-        '"': '',
-        ',': '',
-        '   +': ' +',
-        '   -': ' -',
-        '"true"': 'true',
-        '"false"': 'false',
-        '"null"': 'null',
-    }
-    for key, value in data.items():
-        string = string.replace(key, value)
-    return string
-
-
-def _make_dict(
-        action: str, key: str, value: str, old_value: str | None = None,
-) -> dict:
-    map_dict = {
-        'added': {f'+ {key}': value},
-        'removed': {f'- {key}': value},
-        'updated': {f'- {key}': old_value,
-                    f'+ {key}': value},
-        'unchanged': {key: value}
-    }
-    for key, value in map_dict.items():
-        if key == action:
-            return value
-
-
-def make_stylish(diff: dict) -> str:
-    def walk(diff):
-        result = {}
-
-        for key, value in sorted(diff.items()):
-            if 'children' in value:
-                result[key] = walk(value['children'])
+def to_str(item, depth: int) -> str:
+    if isinstance(item, dict):
+        result = OPEN_BRACE
+        for key, value in item.items():
+            if isinstance(value, dict):
+                upd_value = to_str(value, depth + 1)
+                result += f'{INDENT * depth}{INDENT}{key}:' \
+                          f' {upd_value}\n'
             else:
-                action = value['action']
-                old = value['old_value'] if action == 'updated' else None
-                result.update(
-                    _make_dict(action, key, value['value'], old)
-                )
-        return result
+                result += f'{INDENT * depth}{INDENT}{key}:' \
+                          f' {value}\n'
+        result += f'{INDENT * depth}{CLOSE_BRACE}'
+        return result.strip()
+    if isinstance(item, bool):
+        return str(item).lower()
+    if item is None:
+        return 'null'
+    return item
 
-    result = json.dumps(walk(diff), indent=4)
-    return _convert_string(result)
+
+def make_stylish(tree: dict) -> str:
+    def walk(node, depth):
+        result = OPEN_BRACE
+        for key, value in sorted(node.items()):
+            cur_depth = depth + 1
+            if 'children' in value:
+                result += f'{INDENT * cur_depth}{key}:' \
+                          f' {walk(value.get("children"), cur_depth)}\n'
+            else:
+                new_value = value.get('value')
+                old_value = value.get('old_value')
+                actions = {
+                    'updated': f'{INDENT * depth}'
+                               f'{REMOVE}{key}: '
+                               f'{to_str(old_value, cur_depth)}\n'
+                               f'{INDENT * depth}'
+                               f'{ADD}{key}: '
+                               f'{to_str(new_value, cur_depth)}\n',
+                    'added': f'{INDENT * depth}'
+                             f'{ADD}{key}: '
+                             f'{to_str(new_value, cur_depth)}\n',
+                    'removed': f'{INDENT * depth}'
+                               f'{REMOVE}{key}: '
+                               f'{to_str(new_value, cur_depth)}\n',
+                    'unchanged': f'{INDENT * depth}'
+                                 f'{INDENT}{key}: '
+                                 f'{to_str(new_value, cur_depth)}\n'
+                }
+                result += actions[value.get('action')]
+        result += f'{INDENT * depth}{CLOSE_BRACE}'
+        return result.strip()
+
+    return walk(tree, 0)
